a741909eaf4619653aa5552a3f10ebe588046c8c
praveen-rikhari <praveenrikhari45@gmail.com>
2024-03-14 14:49:52 +0530

    setting up the routes

--- main.go
+++ main.go
@@ -3,13 +3,36 @@ package main
 import (
 	"fmt"
 	"log"
-	"encoding/json"
-	"math/rand"
 	"net/http"
-	"strconv"
+
 	"github.com/gorilla/mux"
 )
 
+type SuperHero struct {
+	ID      int      `json:"id"`
+	Name    string   `json:"name"`
+	Power   string   `json:"power"`
+	Creator *Creator `json:"creator"`
+}
+
+type Creator struct {
+	OrgName  string `json:"orgName"`
+	Category string `json:"category"`
+}
+
+var superHeros []SuperHero
+
 func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/heroes", getHeroes).Methods("GET")
+	r.HandleFunc("/heroes/{id}", getHero).Methods("GET")
+	r.HandleFunc("/heroes", createMovie).Methods("POST")
+	r.HandleFunc("/heroes/{id}", updateHero).Methods("PUT")
+	r.HandleFunc("/heroes/{id}", deleteHero).Methods("DELETE")
+
+	fmt.Printf("Server started at port 8000\n")
+	
+	log.Fatal(http.ListenAndServe(":8000", r))
 	fmt.Println("SuperHeroes CRUD Api")
 }
