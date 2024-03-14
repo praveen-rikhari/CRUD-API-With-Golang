9904707539f22f555f1f5318ac0e37507aeaa294
praveen-rikhari <praveenrikhari45@gmail.com>
2024-03-14 15:43:26 +0530

    creating function for retrieving data

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,7 +10,7 @@ import (
 )
 
 type SuperHero struct {
-	ID      int      `json:"id"`
+	ID      string   `json:"id"`
 	Name    string   `json:"name"`
 	Power   string   `json:"power"`
 	Creator *Creator `json:"creator"`
@@ -22,8 +23,32 @@ type Creator struct {
 
 var superHeros []SuperHero
 
+func getHeroes(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(superHeros)
+}
+
 func main() {
 	r := mux.NewRouter()
+
+	superHeros = append(superHeros, SuperHero{
+		ID:    "1",
+		Name:  "Batman",
+		Power: "Money, Will Power",
+		Creator: &Creator{
+			OrgName:  "DC",
+			Category: "Dark, Crime, Thriller",
+		},
+	})
+	superHeros = append(superHeros, SuperHero{
+		ID:    "2",
+		Name:  "Ironman",
+		Power: "Tech, Suit",
+		Creator: &Creator{
+			OrgName:  "Marvel",
+			Category: "Action, Sci-Fi, Thriller",
+		},
+	})
 
 	r.HandleFunc("/heroes", getHeroes).Methods("GET")
 	r.HandleFunc("/heroes/{id}", getHero).Methods("GET")
@@ -32,7 +57,7 @@ func main() {
 	r.HandleFunc("/heroes/{id}", deleteHero).Methods("DELETE")
 
 	fmt.Printf("Server started at port 8000\n")
-	
+
 	log.Fatal(http.ListenAndServe(":8000", r))
 	fmt.Println("SuperHeroes CRUD Api")
 }
