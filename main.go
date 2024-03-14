b3889cb831b12f1307ba487c23ac4b6cbae2d87c
praveen-rikhari <praveenrikhari45@gmail.com>
2024-03-14 14:05:35 +0530

    Setting up the project structure and installing dependencies

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"encoding/json"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	fmt.Println("SuperHeroes CRUD Api")
+}
